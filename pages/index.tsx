414c758e1c50881344237095bb32528d16943fce
NookieNoo <biv0208@yandex.ru>
2021-05-29 00:35:45 +0300

    feature: Настроили дебаг в vscode

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home(): JSX.Element {
+    const a = 1;
     return (
         <div className={styles.container}>
             <Head>
@@ -71,7 +72,7 @@ export default function Home(): JSX.Element {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Powered by{' '}
+                    Powered by{a}
                     <span className={styles.logo}>
                         <Image
                             src="/vercel.svg"
