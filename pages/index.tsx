9c0dff1c1541b1219b4f737e193477ccaf6a34b8
NookieNoo <biv0208@yandex.ru>
2021-05-30 00:04:40 +0300

    feature: Работающий компонент-кнопка

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
     return (
         <div>
             <Htag tag="h2">Text</Htag>
+            <Button appearance="primary" arrow="right">
+                Кнопка
+            </Button>
+            <Button appearance="ghost" arrow="down">Кнопка</Button>
         </div>
     );
 }
